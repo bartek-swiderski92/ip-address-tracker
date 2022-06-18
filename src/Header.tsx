8f7750bd4a6cde760573177cc8bf48b534272f74
Bartek <phos23@gmail.com>
2022-06-18 14:33:43 +0100

    add bg image

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,13 +1,20 @@
 import './Header.scss'
 
 function Header() {
+    const background = './images/pattern-bg.png'
+
 
     return (
         <header>
-            <div className="background-image">
-                <img className="header-bg-image" src={require('./images/pattern-bg.png')} alt="background" />
+            <div className="background-image"
+                style={{
+                    backgroundImage: "url(" + background + ")"
+                }}
+            >
+                {/* <img className="header-bg-image" src={require('./images/pattern-bg.png')} alt="background" /> */}
+
             </div>
-        </header>
+        </header >
     )
 }
 
