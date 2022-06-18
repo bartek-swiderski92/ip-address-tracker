9570b5fd2acf7e6a98719e4d54a637225c63103d
Bartek Swiderski <b.swiderski92@gmail.com>
2022-06-18 15:18:43 +0100

    styled heading

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,10 +3,12 @@ import './Header.scss'
 function Header() {
     const background = './images/pattern-bg.png'
 
-
     return (
         <header>
-            <div className="background-image">            </div>
+            <div className="header--background">
+                <h1 className="header--heading">IP Address Tracker</h1>
+
+            </div>
         </header >
     )
 }
