c9a523e3684bd9aab5441fd68be9cbaa27ec0ed1
Bartek Swiderski <b.swiderski92@gmail.com>
2022-06-20 15:53:22 +0100

    increase indent

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,31 +12,31 @@ const API_KEY = 'AIzaSyBrsnBRlSG9TfiQQif6GbwzTp7AQtdeyCA'
 
 function App() {
 
-  const [ipResponse, setIpResponse] = useState()
+    const [ipResponse, setIpResponse] = useState()
 
-  return (
-    <div className="app">
-      <Header setIpResponse={setIpResponse} />
+    return (
+        <div className="app">
+            <Header setIpResponse={setIpResponse} />
 
 
 
-      {ipResponse ? (
-        <div className="app__response">
-          <DataDisplay ipResponse={ipResponse} />
-          <MapsFrame ipResponse={ipResponse} API_KEY={API_KEY} />
-        </div >
-      ) : (
-        <>
-          <FontAwesomeIcon className="app__spinner" icon={faSpinner} />
-        </>
-      )
-      }
-      <div className="app__attribution">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.
-        Coded by <a href="https://www.linkedin.com/in/bartek-swiderski92/">Bartek Swiderski</a>.
-      </div>
-    </div>
-  );
+            {ipResponse ? (
+                <div className="app__response">
+                    <DataDisplay ipResponse={ipResponse} />
+                    <MapsFrame ipResponse={ipResponse} API_KEY={API_KEY} />
+                </div >
+            ) : (
+                <>
+                    <FontAwesomeIcon className="app__spinner" icon={faSpinner} />
+                </>
+            )
+            }
+            <div className="app__attribution">
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.
+                Coded by <a href="https://www.linkedin.com/in/bartek-swiderski92/">Bartek Swiderski</a>.
+            </div>
+        </div>
+    );
 }
 
 export default App;
